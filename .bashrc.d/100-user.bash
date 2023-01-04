1105593c52359e842ffc82d98de09d0424a38f7e
Ioannis Bourlakos <jbourlakos@gmail.com>
2023-01-04 12:26:45 +0200

    Revert .bashrc content; add new `gg()` alias-function

--- .bashrc.d/100-user.bash
+++ .bashrc.d/100-user.bash
@@ -11,8 +11,21 @@
 # Setup command line prompt
 #
 
+# Default
 export PROMPT="\[\e[32m\]\u\[\e[m\]@\[\e[32m\]\h\[\e[m\]:\[\e[36m\]\w\[\e[m\]\n$ " # compatible with bash-it
 export PS1="$PROMPT"
+
+# Enable magicmonty/bash-git-prompt
+if [ ! -d $HOME/.bash-git-prompt ]; then
+    echo 'Missing bash-git-prompt.'
+    echo 'Install using: `git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1`'
+else
+    if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
+        GIT_PROMPT_ONLY_IN_REPO=1
+        GIT_PROMPT_THEME="Solarized_Ubuntu"
+        source $HOME/.bash-git-prompt/gitprompt.sh
+    fi
+fi
 
 #
 # Setup utility functions
@@ -32,7 +45,25 @@ function portclosetcp {
 
 function whatismyip {
     # curl --silent checkip.dyndns.org 2>&1 |perl -e 'local $\; print( ( /<body>(.*)<\/body>/si ? $1 : "Not found" ), "\n")';
-    curl --silent checkip.dyndns.org 2>&1 |perl -e 'local $\; print <> =~ /<body>(.*)<\/body>/si ? $1 : "Not found", "\n";';
+    curl --silent checkip.dyndns.org 2>&1 | perl -e 'local $\; print <> =~ /<body>(.*)<\/body>/si ? $1 : "Not found", "\n";'
+}
+
+function gg {
+    if command -v perl >/dev/null 2>&1; then
+        grep_cmd="grep -P"
+    else
+        grep_cmd="grep"
+    fi
+
+    if command -v git >/dev/null 2>&1; then
+        if git rev-parse --git-dir >/dev/null 2>&1; then
+            git "$grep_cmd" "$@"
+        else
+            "$grep_cmd" "$@"
+        fi
+    else
+        "$grep_cmd" "$@"
+    fi
 }
 
 #
@@ -40,7 +71,7 @@ function whatismyip {
 #
 
 alias codedate='\date +"%Y%m%d%H%M%S"'
-alias date='\date +"%Y-%m-%d %H:%M:%S"'
+# alias date='\date +"%Y-%m-%d %H:%M:%S"'
 alias ls='ls --color=auto -a'
 alias vi='vim'
 alias view="vim -M"
